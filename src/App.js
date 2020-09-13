5338732ec83787687e7e9c03bacfb7cebcc24b2b
juliusheese <julius.42.shapiro@gmail.com>
2020-09-13 18:46:18 -0500

    axios done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Form from './pizza/Form';
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <button><Link to="/">Home</Link></button>
+      <button><Link to="/pizza/Form.js">Forums</Link></button>
+      <Form />
     </>
   );
 };
